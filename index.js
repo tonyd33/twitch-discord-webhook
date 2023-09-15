fc61f5d20087f1d5480e1974b737845625b7efb2
Tony Du <tonydu121@hotmail.com>
2023-09-15 13:08:36 -0700

    docker

--- index.js
+++ index.js
@@ -11,24 +11,71 @@ import fsPromises from "fs/promises";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const clientId = process.env["TWITCH_CLIENT_ID"];
-const clientSecret = process.env["TWITCH_CLIENT_SECRET"];
-// testing locally will use ngrok, prod will be behind reverse proxy
-const prod = process.env["PROD"] || false;
-const discordWebhookUrl = prod
-    ? process.env["DISCORD_WEBHOOK_URL"]
-    : process.env["DISCORD_WEBHOOK_URL_TESTING"];
 
-const discordWebhook = new DiscordWebhook(discordWebhookUrl);
-
-const authProvider = new AppTokenAuthProvider(clientId, clientSecret);
-const apiClient = new ApiClient({ authProvider });
-
-const adapter = prod
-    ? new EnvPortAdapter({ apiClient, hostName: "twh.tonydu.me" })
-    : new NgrokAdapter();
+let globals = { loaded: false };
 const secret = crypto.randomBytes(16).toString("hex");
-const listener = new EventSubHttpListener({ apiClient, adapter, secret });
+
+function loadConfig() {
+    const clientId = process.env["TWITCH_CLIENT_ID"];
+    const clientSecret = process.env["TWITCH_CLIENT_SECRET"];
+    // testing locally will use ngrok, prod will be behind reverse proxy
+    const prod = process.env["PROD"] == 1;
+    const discordWebhookUrl = prod
+	? process.env["DISCORD_WEBHOOK_URL"]
+	: process.env["DISCORD_WEBHOOK_URL_TESTING"];
+    const domain = process.env["DOMAIN"];
+    const port = process.env["PORT"] ?? 8080;
+
+    const requiredConfig = [clientId, clientSecret, discordWebhookUrl, domain];
+    const missingConfig = requiredConfig.filter(c => !c);
+
+    if (missingConfig.length > 0) {
+	throw new Error(`${missingConfig.length} entries are missing`);
+    }
+
+    return {
+	clientId,
+	clientSecret,
+	prod,
+	discordWebhookUrl,
+	domain,
+	port
+    }
+}
+
+function loadGlobals() {
+    const {
+	clientId,
+	clientSecret,
+	prod,
+	discordWebhookUrl,
+	domain,
+	port
+    } = loadConfig();
+
+    const discordWebhook = new DiscordWebhook(discordWebhookUrl);
+
+    const authProvider = new AppTokenAuthProvider(clientId, clientSecret);
+    const apiClient = new ApiClient({ authProvider });
+
+    const adapter = prod
+	? new EnvPortAdapter({ apiClient, hostName: domain})
+	: new NgrokAdapter();
+    const listener = new EventSubHttpListener({ apiClient, adapter, secret });
+
+    globals = {
+	...globals,
+	discordWebhook,
+	apiClient,
+	listener,
+
+	prod,
+	domain,
+	port,
+
+	loaded: true,
+    };
+}
 
 /*
  * These are the streams that we want to send notifications for.
@@ -36,14 +83,9 @@ const listener = new EventSubHttpListener({ apiClient, adapter, secret });
  */
 async function getNotifyUsernames() {
     let usernames = [];
-    try {
-        const text = await fsPromises.readFile("./usernames.txt", "utf8");
-        usernames = text.split("\n").filter((username) => !!username);
-    } catch (err) {
-        console.error(err);
-    } finally {
-        return usernames;
-    }
+    const text = await fsPromises.readFile("./usernames.txt", "utf8");
+    usernames = text.split("\n").filter((username) => !!username);
+    return usernames;
 }
 
 /*
@@ -54,10 +96,9 @@ async function getNotifyUsernames() {
  * https://twurple.js.org/reference/eventsub-base/classes/EventSubStreamOnlineEvent.html
  */
 async function subscribeToStreams(usernames, cb) {
-    const users = await apiClient.users.getUsersByNames(usernames);
-    console.log(users.map(({ id }) => id));
+    const users = await globals.apiClient.users.getUsersByNames(usernames);
     const subscriptions = users.map(({ id }) =>
-        listener.onStreamOnline(id, (e) => cb(e))
+        globals.listener.onStreamOnline(id, (e) => cb(e))
     );
     return subscriptions;
 }
@@ -78,7 +119,7 @@ async function sendDiscordWebhook({
         .setThumbnail(profilePictureUrl)
         .setDescription(`**${username} is live!**`)
         .setImage(thumbnailUrl);
-    await discordWebhook.send(embed);
+    await globals.discordWebhook.send(embed);
 }
 
 async function onStreamUp(e) {
@@ -92,10 +133,9 @@ async function onStreamUp(e) {
         }
         const { displayName: username } = user;
         const { gameName, title } = stream;
-        // workaround a bug in twurple api
         const thumbnailUrl = stream.thumbnailUrl.replace(
             "-{width}x{height}",
-            ""
+            `-${1280}x${720}`
         );
         const { profilePictureUrl } = user;
         sendDiscordWebhook({
@@ -111,15 +151,28 @@ async function onStreamUp(e) {
 }
 
 async function main() {
-    console.log(`Starting in prod mode: ${prod}`)
-    await apiClient.eventSub.deleteAllSubscriptions();
-    const usernames = await getNotifyUsernames();
-    const subscriptions = await subscribeToStreams(usernames, onStreamUp);
-    const testCommands = await Promise.all(
-        subscriptions.map((s) => s.getCliTestCommand())
-    );
-    console.log(`Try testing with\n${testCommands.join("\n")}`);
-    listener.start();
+    try {
+	loadGlobals();
+
+	await globals.apiClient.eventSub.deleteAllSubscriptions();
+	const usernames = await getNotifyUsernames();
+
+	if (usernames.length === 0) {
+	    console.error("No usernames specified, exiting...");
+	    process.exit(1);
+	}
+	const subscriptions = await subscribeToStreams(usernames, onStreamUp);
+	const testCommands = await Promise.all(
+	    subscriptions.map((s) => s.getCliTestCommand())
+	);
+	console.log(`Environment mode: ${globals.prod ? "PROD" : "DEV"}`);
+	console.log(`Running on port ${globals.port}`);
+	console.log(`Try testing with\n${testCommands.join("\n")}`);
+	globals.listener.start();
+    } catch (err) {
+	console.error(err);
+	process.exit(1);
+    }
 }
 
 await main();
